af7109a8e3bc943db0e4ad5c727a5267f54675f1
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-15 23:50:17 -0600

    using Greeter component with App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Greeter  from './components/Greeter';
 
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello!</h1>
+     <Greeter/>
     </div>
   );
 }
