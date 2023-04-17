408ea409ca3a33b2e4da7de2a9fc313c08c4d608
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-17 10:26:07 -0600

    models file created

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Greeter  from './components/Greeter';
 import ShoppingList from './components/ShoppingList';
+import Item from './models/item';
+
 
 
 function App() {
  
+  const [items, setItems] = useState<Item[]>([])
 
-  const items = [ 
-    {id: 1,
-    product:"Lemon",
-    quantity: 3},
-    {id: 2,
-    product: "Chicken Breast",
-    quantity: 3}
-  ]
+  // const items = [ 
+  //   {id: 1,
+  //   product:"Lemon",
+  //   quantity: 3},
+  //   {id: 2,
+  //   product: "Chicken Breast",
+  //   quantity: 3}
+  // ]
 
   return (
     <div >
--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -10,14 +10,14 @@ interface ShoppingListProps {
     items: Item[]
 }
 
-export default function ShoppingList(props: ShoppingListProps) : JSX.Element {
+export default function ShoppingList({items}: ShoppingListProps) : JSX.Element {
 
 
     return (
         <div>
             <h1>Shopping List</h1>
             <ul>
-                {props.items.map (item => (
+                {items.map (item => (
                     <li key={item.id}>{item.product} - {item.quantity}</li>
                 ))}
             </ul>
