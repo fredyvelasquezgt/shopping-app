4de48c34943578a3e816ba0a57fae399399ba0d0
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-17 10:43:28 -0600

    using useRef

--- src/components/ShoppingListForm.tsx
+++ src/components/ShoppingListForm.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
 
 function ShoppingListForm(): JSX.Element {
+
+    const inputRef = useRef<HTMLInputElement>(null);
 
     function handleSubmit(e:React.FormEvent) {
         e.preventDefault();
@@ -9,7 +11,7 @@ function ShoppingListForm(): JSX.Element {
     }
 
     return <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Product Name" />
+        <input type="text" placeholder="Product Name" ref={inputRef} />
         <button type="submit">Add Item</button>
     </form>
 }
