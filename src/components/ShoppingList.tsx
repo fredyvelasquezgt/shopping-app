772c6f43f3fbcc96e1bbce22a0527b32dd65ba54
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-16 23:57:08 -0600

    adding html to component

---
+++ src/components/ShoppingList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ShoppingList() : JSX.Element {
+    return (
+        <div>
+            <h1>Shopping List</h1>
+            <ul>
+                <li>Item 1</li>
+            </ul>
+        </div>
+    )
+}
