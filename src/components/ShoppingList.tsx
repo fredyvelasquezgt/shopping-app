43a3744d6e00d6d276a2423aa271060bc6e09aaf
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-17 10:28:04 -0600

    creating shoppinglistform component

--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
+import Item from '../models/item';
 
-interface Item {
-    id: number;
-    product: string;
-    quantity: number
-}
+
+
 
 interface ShoppingListProps {
     items: Item[]
